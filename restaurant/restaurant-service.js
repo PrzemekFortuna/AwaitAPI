6077cf5ee97b586aee734e62db5a321e2cd824a6
Przemek Fortuna <przemyslaw.fortuna@gmail.com>
2019-04-05 18:25:16 +0200

    [REFACTOR] Hashing password in restaurant

--- restaurant/restaurant-service.js
+++ restaurant/restaurant-service.js
@@ -1,8 +1,11 @@
 const Restaurant = require('./restaurant');
+const hashService = require('../utils/hash-service');
 
 exports.createRestaurant = (restaurant) => {
     return new Promise(async (resolve, reject) => {
         try {
+            var hashedPassword = await hashService.hash(restaurant.password);
+            restaurant.password = hashedPassword;
             var newRestarurant = await Restaurant.create(restaurant);
             resolve(newRestarurant);
         } catch(error) {
@@ -12,6 +15,7 @@ exports.createRestaurant = (restaurant) => {
 }
 
 exports.getRestaurant = (id) => {
+    //TODO: return DTO
     return new Promise(async (resolve, reject) => {
         try {
             var restaurant = await Restaurant.findById(id);
@@ -23,6 +27,7 @@ exports.getRestaurant = (id) => {
 }
 
 exports.getAllRestaurantsFromLocation = (locId) => {
+    //TODO: return DTOs
     return new Promise(async (resolve, reject) => {
         try {
             let restaurants = await Restaurant.find({ location: locId });
--- restaurant/restaurant.js
+++ restaurant/restaurant.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 
 var restaurant = mongoose.Schema({
-   Name: { type: Schema.Types.String, require: true},
-   Email: {type: Schema.Types.String},
-   Location: {type: Schema.Types.ObjectId, ref:'Location'},
-   Password: {type: Schema.Types.String, require: true}
+   name: { type: Schema.Types.String, require: true},
+   email: {type: Schema.Types.String},
+   location: {type: Schema.Types.ObjectId, ref:'Location'},
+   password: {type: Schema.Types.String, require: true}
 });
 
 module.exports = mongoose.model("Restaurant", restaurant);
