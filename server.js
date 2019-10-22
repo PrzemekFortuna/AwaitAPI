133b966a7e0b891d030587d7629491e632f5bdcf
Przemek Fortuna <przemyslaw.fortuna@gmail.com>
2019-10-22 20:03:40 +0200

    Configured Swagger

--- server.js
+++ server.js
@@ -2,9 +2,26 @@ var express = require('express');
 const db = require('./db/db');
 var app = express();
 const hash = require('./utils/hash-service');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
+
+let swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            title: 'Await API',
+            description: 'Await REST API documentation'
+        },
+        servers: ["http://localhost:3000"]
+    },
+    apis: ["server.js"]
+};
 
 
 const port = process.env.port || 3000;
+
+// SWAGGER
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 const restaurantsController = require('./restaurant/restaurant-controller');
 app.use('/restaurants', restaurantsController);
